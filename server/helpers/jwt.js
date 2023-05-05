4ee88f4bef6c018f3d92c6fca9911a826756a856
Randy Ramadhan <97931643+rann02@users.noreply.github.com>
2023-05-05 16:00:05 +0700

    Delete server directory

--- server/helpers/jwt.js
+++
@@ -1,7 +0,0 @@
-const jwt = require('jsonwebtoken');
-const secret = process.env.SECRET
-
-module.exports = {
-    createToken: (payload) => jwt.sign(payload, secret),
-    verifyToken: (token) => jwt.verify(token, secret)
-}
--- server/routes/user.js
+++
@@ -1,8 +0,0 @@
-const express = require('express')
-const router = express.Router()
-const Controller = require('../controllers/userController')
-
-router.post('/register', Controller.postUser)
-router.post('/login', Controller.login)
-
-module.exports = router
